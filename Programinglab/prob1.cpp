4e17a73f25a2d586831a16dc3899c3c72b1868f2
Tanmay Bhadviya <122905596+tanmay-6@users.noreply.github.com>
2025-08-01 04:23:42 +0000

    Quest1

--- Programinglab/prob1.cpp
+++ Programinglab/prob1.cpp
@@ -1,6 +1,150 @@
 #include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 typedef pair<int,int> pii;
+
+
+void djk(int src, vector<vector<pii>> &adj){
+    vector<int> dist(adj.size(), INT_MAX);
+    vector<int> parent(adj.size(),-1);
+    
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    dist[src] = 0;
+    pq.push({0,src});
+    
+    while(!pq.empty()){
+        int dis = pq.top().first;
+        int node = pq.top().second;
+        pq.pop();
+
+        for(auto it : adj[node]){
+            int ew = it.second; //ew -> edgeweight
+            int adjnode = it.first;
+            
+            if(dis + ew < dist[adjnode]){
+                dist[adjnode] = dis + ew;
+                parent[adjnode] = node;
+                pq.push({dist[adjnode], adjnode});
+            }
+        }
+    }
+
+    for(int i=1; i<adj.size(); i++){
+        cout<<parent[i]<<" "<<i<<endl;
+    }
+}
+
+//BFS
+void bfs(int src, vector<vector<int>> &adj){
+    queue<int> q;
+    vector<int> visited(adj.size(),0);
+    visited[src] = 1;
+    int level = 0;
+    q.push(src);
+    while(!q.empty()){
+        int size = q.size();
+        for(int i=0; i<size; i++){
+            int top = q.front();
+            cout<<top<<endl;
+            q.pop();
+            for( auto neigh : adj[top]){
+                if(visited[neigh] == 0){
+                    q.push(neigh);
+                    visited[neigh] = 1;
+                }
+            }
+        }
+        level++;
+    }
+}
+
+//DFS
+void dfs(int src, vector<vector<int>> &adj){
+    stack<int> st;
+    vector<int> visited(adj.size(), 0);
+    st.push(src);
+    visited[src] = 1;
+
+    while(!st.empty()){
+        int node = st.top();
+        st.pop();
+        cout<<node<<endl;
+
+        for(auto it : adj[node]){
+            if(visited[it] == 0){
+                st.push(it);
+                visited[it] = 1;
+            }
+        }
+        
+    }
+    cout<<endl;
+}
+
+//0/1BFS
+void wbfs(int src, vector<vector<pii>> &adj){
+    vector<int> dist(adj.size(), INT_MAX);
+    vector<int> parent(adj.size(), -1);
+    dist[src] = 0;
+    deque<int> dq;
+    dq.push_front(src);
+    
+    while(!dq.empty()){
+        int node = dq.front();
+        dq.pop_front();
+        for( auto it : adj[node]){
+            int adjnode = it.first;
+            int dis = it.second;
+            
+            if(dis + dist[node] < dist[adjnode]){
+                dist[adjnode] = dis + dist[node];
+                parent[adjnode] = node;
+                
+                if(dis == 0) dq.push_front(adjnode);
+                else dq.push_back(adjnode);
+            }
+        }
+    }
+    
+    for(int i=0; i<adj.size(); i++){
+        cout<<i<<" "<<dist[i]<<" "<<parent[i]<<endl;
+    }
+}
+
+//Prim's 
+void prm(int src, vector<vector<pii>> &adj){
+    vector<int> wgt(adj.size(), INT_MAX), parent(adj.size(), -1);
+    vector<int> visited(adj.size(), 0);
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    wgt[0] = 0;
+    pq.push({0,0});
+    
+    while(!pq.empty()){
+        int node = pq.top().second;
+        pq.pop();
+        visited[node] = true;
+        
+        for(auto it : adj[node]){
+            int ew = it.second;
+            int adjnode = it.first;
+            
+            if(visited[adjnode] == 0 && ew<wgt[adjnode]){
+                wgt[adjnode] = ew;
+                parent[adjnode] = node;
+                pq.push({wgt[adjnode], adjnode});
+            }
+        }
+    }
+    cout << "E - W"<<endl;
+    int total = 0;
+    for (int i = 1; i < adj.size(); ++i) {
+        if (parent[i] != -1) {
+            cout << parent[i] << " " << i << " " << wgt[i] << "\n";
+            total += wgt[i];
+        }
+    }
+    cout<<"Total: "<<total<<endl;
+}
 
 int main(){
     int n,m;
@@ -73,143 +217,4 @@ int main(){
     #endif
 
     return 0;
-}
-
-void djk(int src, vector<vector<pii>> &adj){
-    vector<int> dist(adj.size(), INT_MAX);
-    vector<int> parent(adj.size(),-1);
-
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    dist[src] = 0;
-    pq.push({0,src});
-
-    while(!pq.empty()){
-        int dis = pq.top().first;
-        int node = pq.top().second;
-        pq.pop();
-
-        for(auto it : adj[node]){
-            int ew = it.second; //ew -> edgeweight
-            int adjnode = it.first;
-
-            if(dis + ew < dist[adjnode]){
-                dist[adjnode] = dis + ew;
-                parent[adjnode] = node;
-                pq.push({dist[adjnode], adjnode});
-            }
-        }
-    }
-
-    for(int i=0; i<adj.size(); i++){
-        cout<<i<<" "<<dist[i]<<" "<<parent[i]<<endl;
-    }
-}
-
-//BFS
-void bfs(int src, vector<vector<int>> &adj){
-    queue<int> q;
-    vector<int> visited(adj.size(),0);
-    visited[src] = 1;
-    int level = 0;
-    q.push(src);
-    while(!q.empty()){
-        int size = q.size();
-        for(int i=0; i<size; i++){
-            int top = q.front();
-            cout<<top<<endl;
-            q.pop();
-            for( auto neigh : adj[top]){
-                if(visited[neigh] == 0){
-                    q.push(neigh);
-                    visited[neigh] = 1;
-                }
-            }
-        }
-        level++;
-    }
-}
-
-//DFS
-void dfs(int src, vector<vector<int>> &adj){
-    stack<int> st;
-    st.push(src);
-    vector<int> visited(adj.size(), 0);
-    visited[src]=1;
-    while(!st.empty()){
-        int top = st.top();
-        st.pop();
-        
-        cout<<top<<" ";
-        for(auto it : adj[top]){
-            if(visited[it] == 0){
-                st.push(it);
-                visited[it] = 1;
-            }
-        }
-    }
-}
-
-//0/1BFS
-void wbfs(int src, vector<vector<pii>> &adj){
-    vector<int> dist(adj.size(), INT_MAX);
-    vector<int> parent(adj.size(), -1);
-    dist[src] = 0;
-    deque<int> dq;
-    dq.push_front(src);
-
-    while(!dq.empty()){
-        int node = dq.front();
-        dq.pop_front();
-        for( auto it : adj[node]){
-            int adjnode = it.first;
-            int dis = it.second;
-
-            if(dis + dist[node] < dist[adjnode]){
-                dist[adjnode] = dis + dist[node];
-                parent[adjnode] = node;
-
-                if(dis == 0) dq.push_front(adjnode);
-                else dq.push_back(adjnode);
-            }
-        }
-    }
-
-    for(int i=0; i<adj.size(); i++){
-        cout<<i<<" "<<dist[i]<<" "<<parent[i]<<endl;
-    }
-}
-
-//Prim's 
-void prm(int src, vector<vector<pii>> &adj){
-    vector<int> keys(adj.size(), INT_MAX), parent(adj.size(), -1);
-    vector<int> visited(adj.size(), 0);
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    keys[0] = 0;
-    pq.push({0,0});
-
-    while(!pq.empty()){
-        int node = pq.top().second;
-        pq.pop();
-        visited[node] = true;
-
-        for(auto it : adj[node]){
-            int ew = it.second;
-            int adjnode = it.first;
-
-            if(visited[adjnode] == 0 && ew<keys[adjnode]){
-                keys[adjnode] = ew;
-                parent[adjnode] = node;
-                pq.push({keys[adjnode], adjnode});
-            }
-        }
-    }
-    cout << "Edge   Weight\n";
-    int total = 0;
-    for (int i = 1; i < adj.size(); ++i) {
-        if (parent[i] != -1) {
-            cout << parent[i] << " - " << i << "   " << keys[i] << "\n";
-            total += keys[i];
-        }
-    }
-    cout << "Total Weight: " << total << "\n";
 }

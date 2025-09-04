6cdad27b01d75743a9e6f56a8435bcd9454a4cb7
Tanmay Bhadviya <122905596+tanmay-6@users.noreply.github.com>
2025-09-04 20:55:35 +0000

    done assignment 6

--- Programinglab/tut5/Prob4.c
+++ Programinglab/tut5/Prob4.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
-#include <stdint.h>
 int *p;
 int val;
 
 void foo(int a){
   p = &a;
-  val = *(--p);
+  p += 3;
+  val = *(p);
   printf("In foo\n");
 }
 
 
 void bar(int b){
   p = &b;
+  p += 4;
   *(--p) = val;
   printf("In bar\n");
 }
 
 int main(){
-  uintptr_t address = 0x7fffffffceb8;
-  long int *ptr = (long int *)address;
-  *ptr = 0x000055555555522e;
-  //0x000055555555522e 
   int a = 1, b = 2;
   foo(a);
   bar(b);

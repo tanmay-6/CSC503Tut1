a3ba08e2b9a79087f7d763b133194e3282d1ddf4
Tanmay Bhadviya <122905596+tanmay-6@users.noreply.github.com>
2025-09-30 16:25:54 +0000

    tut6 q2 completed

--- Practice/demo.cpp
+++ Practice/demo.cpp
@@ -1,73 +1,18 @@
+#include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
-void merge(vector<int> &arr, int low, int mid, int high){
-    vector<int> brr;
-    int i=low;
-    int j=mid+1;
-    while(i<=mid && j<=high){
-        if(arr[i]<=arr[j]){
-            brr.push_back(arr[i++]);
-        }
-        else{
-            brr.push_back(arr[j++]);
-        }
-    }
-    while(i<=mid){
-        brr.push_back(arr[i++]);
-    }
-    while(j<=high){
-        brr.push_back(arr[j++]);
-    }
-    for(int a=0; a<brr.size(); a++){
-        arr[low+a] = brr[a];
-    }
-}
-
-void mergesort(vector<int> &arr, int low, int high){
-    if(low >= high) return;
-    int mid = low + (high-low)/2;
-    mergesort(arr, low, mid);
-    mergesort(arr, mid+1, high);
-    merge(arr, low, mid, high);
-}
-
-int partition(vector<int> &arr, int low, int high){
-    int pivot = arr[low];
-    int i = low;
-    int j = high;
-
-    while(i<j){
-        while(arr[i]<= pivot && i<high){
-            i++;
-        }
-        while(arr[j]> pivot && j>low){
-            j--;
-        }
-        if(i<j) swap(arr[i], arr[j]);
-    }
-    swap(arr[low], arr[j]);
-    return j;
-}
-
-void quicksort(vector<int> &arr, int low, int high){
-    if(low < high){
-        int partitionIndex = partition(arr, low, high);
-        quicksort(arr, low, partitionIndex-1);
-        quicksort(arr, partitionIndex+1, high);
-    }
-}
-
 void heapify(vector<int> &arr, int n, int i){
     int largest = i;
-    int left = 2*i+1;
-    int right = 2*i+2;
-    if(left <n && arr[left] > arr[largest]){
-        largest = left;
+    int l = 2*i+1;
+    int r = l+1;
+    if(l<n && arr[l]>arr[largest]){
+        largest = l;
     }
-    if(right<n && arr[right] > arr[largest]){
-        largest = right;
+    if(r<n && arr[r]>arr[largest]){
+        largest = r;
     }
+
     if(largest != i){
         swap(arr[i], arr[largest]);
         heapify(arr, n, largest);
@@ -75,20 +20,22 @@ void heapify(vector<int> &arr, int n, int i){
 }
 
 void heapsort(vector<int> &arr, int n){
-    for(int i=n/2; i>=0; i--){
+    for(int i = n/2 -1; i>=0; i--){
         heapify(arr, n, i);
     }
-    for(int i=arr.size()-1; i>0; i--){
+
+    for(int i=n-1; i>0; i--){
         swap(arr[i], arr[0]);
         heapify(arr, i, 0);
     }
 }
 
 int main(){
-    vector<int> arr = {6,5,21,16,32,33,12,9,48,65,76,28};
-    //mergesort(arr, 0, arr.size()-1);
-    //quicksort(arr, 0, arr.size()-1);
+    vector<int> arr = {5,3,4,6,9,10,8};
+    for(int i=0; i<arr.size(); i++) cout<<arr[i]<<" ";
+    cout<<endl;
     heapsort(arr, arr.size());
     for(int i=0; i<arr.size(); i++) cout<<arr[i]<<" ";
+    cout<<endl;
     return 0;
-}
+}   
